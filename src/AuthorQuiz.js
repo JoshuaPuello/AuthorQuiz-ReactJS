96a7baba4ef19557fcac740250b8687bca5e14ba
Joshua Puello <joshuapuello@outlook.com>
2020-04-09 12:26:11 -0500

    Files renamed and bootstrap css file

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import './bootstrap.min.css'
+
+function AuthorQuiz() {
+  return (
+    <div className="container-fluid"></div>
+  );
+}
+
+export default AuthorQuiz;
